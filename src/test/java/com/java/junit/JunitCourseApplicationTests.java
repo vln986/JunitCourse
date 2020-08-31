19c6b331de1821a57dee6c00314970a5762867c9
laxminarayana <Laxminarayana_Veggal@dellteam.com>
2020-08-31 22:12:58 +0530

    Initial commit JunitCourse

---
+++ src/test/java/com/java/junit/JunitCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
