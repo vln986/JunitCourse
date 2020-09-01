418b64ed9aef1518553d65caae0587000bf3d2fa
Laxman <laxminarayanaveggalam@gmail.com>
2020-09-01 09:05:47 +0530

    Junit test suite

---
+++ src/test/java/com/java/junit/AllTests.java
@@ -0,0 +1,11 @@
+package com.java.junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ StringHelperParameterizedTest.class })
+public class AllTests {
+
+}
