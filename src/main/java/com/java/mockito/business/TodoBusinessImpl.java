89d665da5d62429a366e251dfc16bb9a65fdd93e
Laxman <laxminarayanaveggalam@gmail.com>
2020-09-02 11:52:14 +0530

    BusinessImpl

---
+++ src/main/java/com/java/mockito/business/TodoBusinessImpl.java
@@ -0,0 +1,29 @@
+package com.java.mockito.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.java.mockito.service.TodoService;
+
+public abstract class TodoBusinessImpl {
+	
+	private TodoService todoService;
+	
+	public TodoBusinessImpl(TodoService todoService) {
+		this.todoService = todoService;
+	}
+	
+	public List<String> retrieveTodoRelatedToSpring(String user){
+		List<String> filteredTodos = new ArrayList<String>();
+		List<String> todos = todoService.retrieveTodos(user);
+		
+		for(String todo:todos) {
+			if(todo.contains("Spring")) {
+				filteredTodos.add(todo);
+			}
+		}
+		
+		return filteredTodos;
+	}
+
+}
